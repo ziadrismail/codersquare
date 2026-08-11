856c130ddf4ae6b9775a984b4a51c0796136906a
ziad <ziad.r.saad@gmail.com>
2026-08-11 19:50:56 +0300

    in memory datastore

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,24 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
+import { db } from './datastore'
 
 const app = express();
 
 app.use(express.json());
 
-const posts: any[] = [];
+const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
+  console.log(req.method, req.path, '- body:', req.body);
+  next();
+};
+
+app.use(requestLoggerMiddleware);
 
 app.get('/posts', (req, res) => {
-    res.send({ posts });
+    res.send({ posts: db.listPosts() });
 })
 
 app.post('/posts', (req, res) => {
     const post = req.body;
-    posts.push(post);
+    db.createPost(post);
     res.sendStatus(201);
 })
 
